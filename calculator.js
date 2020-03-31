59807f48b5e48e46eeddae686da3f13ade1a5393
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-31 11:06:43 -0700

    added the operators as consts ans wrote pseudo code for the main

--- calculator.js
+++ calculator.js
@@ -1,5 +1,40 @@
+
+//hard code the operators
 const exampleAdditionInput = {
   num1: 3,
   num2: 5,
-  operation: 'add',
+  operation: "add",
+  operation: "+",
 }
+const exampleSubtractionInput = {
+  num1: 3,
+  num2: 5,
+  operation: "subtract",
+}
+const exampleMultiplicationInput = {
+  num1: 3,
+  num2: 5,
+  operation: "multiply",
+}
+const exampleDivisionInput = {
+  num1: 3,
+  num2: 5,
+  operation: "divide",
+}
+
+//prompts for user input
+
+console.log("Welcome to my first JavaScript calculator.")
+
+const calculate = function(user_input) {
+}
+
+
+//validate user input 
+
+//throw NaN
+
+//validate operators
+
+//print formula
+console.log($)
